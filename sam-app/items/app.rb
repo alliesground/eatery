e0f5e612e2e3e0f1881a816faedaced47e52ee1c
alliesground <sameer.libang@gmail.com>
2021-01-17 20:35:16 +1100

    adding psql gem with native extensions and adding docker-compose file for postgres service

--- sam-app/items/app.rb
+++ sam-app/items/app.rb
@@ -3,6 +3,7 @@ require 'aws-sdk-s3'
 require 'json'
 require 'base64'
 require './util'
+require 'pg'
 
 def create(event:, context:)
 
@@ -16,6 +17,14 @@ def create(event:, context:)
 
   # Get Uploaded files url from the file's key passed from client
   # Save Item data with the S3 file url to psql
+
+  conn = PG.connect(dbname: 'eatery', 
+                    user: 'eatery', 
+                    host: 'db', 
+                    port: 5432, 
+                    password: '')
+
+  res = conn.exec( "SELECT count(*) from items" )
 
   {
     statusCode: 200,
