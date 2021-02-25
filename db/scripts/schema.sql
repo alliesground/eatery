01c5505f8e0a83d578114e9c250bc362849a96e7
alliesground <sameer.libang@gmail.com>
2021-02-25 21:06:10 +1100

    db: adding images table

--- db/scripts/schema.sql
+++ db/scripts/schema.sql
@@ -4,3 +4,14 @@ CREATE TABLE IF NOT EXISTS items (
   description varchar(20),
   price numeric(8, 2)
 );
+
+CREATE TABLE IF NOT EXISTS images (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  item_id INT,
+  url TEXT,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_item
+    FOREIGN KEY(item_id)
+      REFERENCES items(id)
+      ON DELETE CASCADE
+);
